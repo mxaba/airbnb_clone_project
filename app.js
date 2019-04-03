e2b2b3e3bebd38de68247d290e69b3d57bf69d1f
mxaba <samuel.mcebo@gmail.com>
2019-04-03 13:49:05 +0200

    A lot need to be done

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const cookieSession = require('cookie-session')
 var index = require('./routes/index');
+const keys = require('./config/keys');
+var passport = require('passport');
+var mongoose = require('mongoose');
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// set up session cookies
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb');
+});
+
 // set path for static assets
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'models')));
+app.use(express.static(path.join(__dirname, 'config')));
+
 
 
 // routes
---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: '798701584187-pjfogdt5gjk1it4bsh1o0b25q1dkcg1i.apps.googleusercontent.com',
+        clientSecret: 'Z1Q707hwxNzSPVkAvDXc3qxs'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://mxaba:uozkifovRKJCBvXV@airbnbdb-yucsr.mongodb.net/test?retryWrites=true'
+    },
+    session: {
+        cookieKey: 'thesegfaultisawesomeiguess'
+    }
+};
