b8375d06a012d4e08737eb3484308c25a19f6874
Lisbert Drey SIBANDA <lsibanda@e5r10p1.wethinkcode.co.za>
2019-04-15 12:14:51 +0200

    firstcommit

---
+++ test/demo_test.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+// Describe Test
+describe('some demo test', function(){
+    
+    //create some test
+    it('Add two numbers together', function(){
+
+        assert(2 + 3 === 5);
+    });
+});
