7fab46ff14b1836d40c370fba069bd747a27ffba
Mcebo XABA <mxaba@e4r13p1.wethinkcode.co.za>
2019-05-13 09:44:11 +0200

    yeyi

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('dashboard', {page:'Dashboard', menuId:'dashboard', user: req.user} );
-});
+const authCheck = (req, res, next) => {
+  if(!req.user){
+      res.redirect('/auth/login');
+  } else {
+      next();
+  }
+};
 
-router.get('/', function(req, res, next) {
-  res.render('dashboard-messages', {page:'Dashboard-messages', menuId:'dashboard-messages', user: req.user} );
+router.get('/', authCheck, (req, res) => {
+  res.render('dashboard', { user: req.user });
 });
 
 module.exports = router;
--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('dashboard-messages', {page:'Dashboard-messages', menuId:'dashboard-messages', user: req.user} );
-});
+
+const authCheck = (req, res, next) => {
+    if(!req.user){
+        res.redirect('/auth/login');
+    } else {
+        next();
+    }
+  };
+
+  router.get('/', authCheck, (req, res) => {
+    res.render('dashboard-messages', { user: req.user });
+  });
+
+// router.get('/', function(req, res, next) {
+//   res.render('dashboard-messages', {page:'Dashboard-messages', menuId:'dashboard-messages', user: req.user} );
+// });
 
 module.exports = router;
